720fd25733d28da47f1cefcd0ff33702d81ba1d5
phanmduong <phanminhduong@hotmail.com>
2017-02-11 14:38:12 +0700

    change Nakata to Clash add background

--- js/main.js
+++ js/main.js
@@ -1,43 +1,47 @@
-var Nakama = {};
-Nakama.configs = {};
+var Clash = {};
+Clash.configs = {};
 
-window.onload = function(){
-  Nakama.game = new Phaser.Game(640,960,Phaser.AUTO,'',
-    {
-      preload: preload,
-      create: create,
-      update: update,
-      render: render
-    }, false, false
-  );
+window.onload = function () {
+    Clash.game = new Phaser.Game(512, 512, Phaser.AUTO, '',
+        {
+            preload: preload,
+            create: create,
+            update: update,
+            render: render
+        }, false, false
+    );
 }
 
 // preparations before game starts
-var preload = function(){
-  Nakama.game.scale.minWidth = 320;
-  Nakama.game.scale.minHeight = 480;
-  Nakama.game.scale.maxWidth = 640;
-  Nakama.game.scale.maxHeight = 960;
-  Nakama.game.scale.pageAlignHorizontally = true;
-  Nakama.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+var preload = function () {
+    Clash.game.scale.minWidth = 512;
+    Clash.game.scale.minHeight = 512;
+    Clash.game.scale.maxWidth = 1024;
+    Clash.game.scale.maxHeight = 1024;
+    Clash.game.scale.pageAlignHorizontally = true;
+    Clash.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
-  Nakama.game.time.advancedTiming = true;
+    Clash.game.time.advancedTiming = true;
 
-  Nakama.game.load.atlasJSONHash('assets', 'Assets/assets.png', 'Assets/assets.json');
-  Nakama.game.load.image('background', 'Assets/Map1.png');
+    Clash.game.load.atlasJSONHash('assets', 'Assets/assets.png', 'Assets/assets.json');
+    Clash.game.load.image('background', 'Assets/background/space1.jpg');
 }
 
 // initialize the game
-var create = function(){
-  Nakama.game.physics.startSystem(Phaser.Physics.ARCADE);
-  Nakama.keyboard = Nakama.game.input.keyboard;
-  Nakama.game.sprite.add()
+var create = function () {
+    Clash.game.physics.startSystem(Phaser.Physics.ARCADE);
+    Clash.keyboard = Clash.game.input.keyboard;
+
+    Clash.background = Clash.game.add.tileSprite(0, 0, 512 , 512, 'background');
+
+    Clash.game.sprite.add()
 }
 
 // update game state each frame
-var update = function(){
+var update = function () {
 
 }
 
 // before camera render (mostly for debug)
-var render = function(){}
+var render = function () {
+}
