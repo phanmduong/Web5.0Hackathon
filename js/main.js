d3cf2f7365419fda2147512afa9c78b2768010b8
HieuNT <hieuntse03782@fpt.edu.vn>
2017-02-14 04:05:33 +0700

    14/02 - created group bullet - create function fire (error)

--- js/main.js
+++ js/main.js
@@ -28,41 +28,37 @@ var preload = function () {
     Clash.game.load.image('background', 'Assets/background/space1.jpg');
 }
 
+var bullets;
+var fireRate = 100;
+var nextFire = 0;
+
 // initialize the games
 var create = function () {
     Clash.game.physics.startSystem(Phaser.Physics.ARCADE);
     Clash.keyboard = Clash.game.input.keyboard;
 
     Clash.background = Clash.game.add.tileSprite(0, 0, 1024, 1024, 'background');
-
     Clash.earth = new Earth(Clash.game.height / 2, Clash.game.width / 2, "base.png");
 
     Clash.ship = Clash.game.add.sprite(Clash.game.height / 2, Clash.game.width / 2 - Clash.earth.sprite.width / 2, "assets", "player1.png");
     Clash.ship.anchor = new Phaser.Point(0.5, 0.5);
 
-
+    Clash.playerBulletGroup = Clash.game.add.physicsGroup();
     //Mọi công việc làm trước hàm này
     createDisplay();
 }
 
 var createDisplay = function () {
     Clash.display.weapon  = createObjectDisplay({x:70, y:70},"cannon1.jpg");
-
     Clash.display.frameWeapon = createObjectDisplay({x:70, y:70},"weapon.png");
-
     Clash.display.frameWeapon.scale.setTo(1.6, 1.6);
-
     Clash.display.iconEarthHP = createObjectDisplay({x:150, y:190},"hp-detail.png");
-
     Clash.display.iconEarthXP = createObjectDisplay({x:165, y:220},"xp-detail.png");
-
     Clash.display.iconEarth = createObjectDisplay({x:70, y:190},"planet-small.png");
-
     Clash.display.iconShipHP = createObjectDisplay({x:150, y:280},"hp-detail.png");
-
     Clash.display.iconShipXP = createObjectDisplay({x:165, y:310},"xp-detail.png");
-
     Clash.display.iconShip = createObjectDisplay({x:70, y:280},"player1.png");
+
 }
 
 var createObjectDisplay= function(position, spriteName){
@@ -83,24 +79,19 @@ var update = function () {
 
 function fire() {
 
-    if (Clash.game.time.now > nextFire && bullets.countDead() > 0)
-    {
-        nextFire = Clash.game.time.now + fireRate;
 
-        var bullet = bullets.getFirstDead();
+   Clash.game.physics.arcade.moveToPointer(Clash.ship, 300);
 
-        bullet.reset(sprite.x - 8, sprite.y - 8);
-
-        Clash.game.physics.arcade.moveToPointer(bullet, 300);
-    }
 
 }
+
+function createBullet(direction){
+    new PlayerBulletType1Controller(
+      Clash.game.input.pointer.position,
+      direction
+    );
+  }
 // before camera render (mostly for debug)
 var render = function () {
     Clash.game.debug.spriteInfo(Clash.ship , 32, 100);
-}
-
-// before camera render (mostly for debug)
-var render = function () {
-    // Clash.game.debug.body(Clash.earth.sprite);
 }
