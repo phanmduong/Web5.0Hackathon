95eb9084fb25b1d257921661960a572cdc6c611e
HieuNT <hieuntse03782@fpt.edu.vn>
2017-02-15 03:56:12 +0700

    15/02

---
+++ js/ShipController.js
@@ -0,0 +1,14 @@
+class ShipController {
+    constructor(x, y, spriteName, configs) {
+        this.sprite = Clash.game.add.sprite(
+            x,
+            y,
+            "assets",
+            spriteName
+        );
+
+        this.sprite.anchor = new Phaser.Point(0.5, 0.5);
+        // this.sprite.body.setCircle(50, this.sprite.width / 2 - 50,
+        //     this.sprite.height / 2 - 50);
+    }
+}
